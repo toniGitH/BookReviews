ecda1882a187312d5d0a4349639b683bcee274a2
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-07-22 09:52:21 +0200

    Registrado BookSeeder en DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Book;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            BookSeeder::class,
+            // Add other seeders here as needed
         ]);
     }
 }
