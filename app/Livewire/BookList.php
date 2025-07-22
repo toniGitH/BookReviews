acf54b55b7070a7d2447063fb76e816d76b69e8a
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-07-22 10:35:45 +0200

    Creando lista de libros en el componente BookList: - en el componente de clase BookList, se ha añadido la propiedad $name con el valor 'Antonio'. - el método render del componente BookList se ha modificado para devolver la vista 'livewire.book-list' con los libros obtenidos del modelo Book. - en el componete de vista se ha recuperado el nombre almacenado en $name en la clase, para el saludo inicial - en el componente de vista se ha añadido un bucle foreach para mostrar los libros obtenidos del modelo Book. - en el componente de vista, en los elementos que se repiten en el bucle, es decir, en los elementos <li>, se ha añadido la directiva wire:key para mejorar el rendimiento y evitar problemas de renderizado.

--- app/Livewire/BookList.php
+++ app/Livewire/BookList.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Book;
 use Livewire\Component;
 
 class BookList extends Component
 {
+    public $name = 'Antonio';
     public function render()
     {
-        return view('livewire.book-list');
+        return view('livewire.book-list', [
+            'books' => Book::all(),
+        ]);
     }
 }
--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <header>
+        <div>
+            <h2>¡Hola, {{ $name }}!</h2>
+            <p>Esta es la lista de tus reviews de libros</p>
+        </div>
+    </header class="flex justify-between">
+    <ul class="list">
+        @foreach($books as $book)
+            <li wire:key="book-{{ $book->id }}">
+                <h3>{{ $book->title }}</h3>
+                <h4>{{ $book->author }}</p>
+                <p>Rating: {{ $book->review }}/10</p>
+            </li>
+        @endforeach
+    </ul>
 </div>
