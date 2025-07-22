142cb642bc2c810737555baa29c5ffac63cbd90e
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-07-22 13:29:47 +0200

    Funcionalidad para eliminar libros - definimos en el componente de clase una función pública llamada delete() que se encarga de eliminar el libro recibido por parámetro de la base de datos. - en la vista del componente creamos, para cada libro (dentro del bucle foreach) un botón que enlazamos con esa función delete() pasándole la id de cada respectivo libro

--- app/Livewire/BookList.php
+++ app/Livewire/BookList.php
@@ -8,6 +8,11 @@ use Livewire\Component;
 class BookList extends Component
 {
     public $name = 'Antonio';
+
+    public function delete(Book $book){
+        $book->delete();
+    }
+
     public function render()
     {
         return view('livewire.book-list', [
--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -8,9 +8,10 @@
     <ul class="list">
         @foreach($books as $book)
             <li wire:key="book-{{ $book->id }}">
+                <button wire:click="delete({{ $book->id }})">Eliminar</button>
                 <h3>{{ $book->title }}</h3>
                 <h4>{{ $book->author }}</p>
-                <p>Rating: {{ $book->review }}/10</p>
+                <p>Rating: {{ $book->rating }}/10</p>
             </li>
         @endforeach
     </ul>
