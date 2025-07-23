07bdb431fcc410dca17ca73ab5622f5b3874f68d
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-07-23 20:50:59 +0200

    Sustituido el bloque <header> por el nuevo componente <livewire:page-header/>

--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -1,16 +1,7 @@
-<div>
-    <header class="flex justify-between">
-        <div>
-            <h2>¡Hola, {{ $name }}!</h2>
-            <p>Esta es la lista de tus reviews de libros</p>
-        </div>
-        <form>
-            <span class="mr-2">Tu nombre:</span>
-            <input type="text" wire:model.live.debounce.500ms="name">
-            <button>Actualizar</button>
-        </form>
-    </header>
-        
+<div> 
+
+    <livewire:page-header /> 
+     
     <ul class="list">
         @foreach($books as $book)
             <li wire:key="book-{{ $book->id }}">
