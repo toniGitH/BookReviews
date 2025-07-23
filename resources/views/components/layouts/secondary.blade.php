3be096bd9e6d6be5bb96469b17c14285bfc1c1e8
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-07-23 20:24:44 +0200

    Creado layout secundario para albergar componentes livewire de página completa

---
+++ resources/views/components/layouts/secondary.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Page Title' }}</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+      <nav>
+        <div>
+          <h1>Blog</h1>
+          <a 
+            @class(['active' => request()->is('/')]) 
+            href="/">
+            Lista de libros
+          </a>
+          <a 
+            @class(['active' => request()->is('create')]) 
+            href="/create">
+            Añadir libro
+          </a>
+          <a 
+            @class(['active' => request()->is('blog')]) 
+            href="/blog">
+            Blog
+          </a>
+        </div>
+      </nav>
+      <main>
+        {{ $slot }}
+      </main>
+    </body>
+</html>
